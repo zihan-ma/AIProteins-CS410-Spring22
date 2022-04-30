cceff5c4c3acaa38f96981d0d2bdef6aa8541d4b
Glisync-00 <William.Hem001@umb.edu>
2022-04-30 14:20:56 -0400

    Updated Disulfide_Parser.py
    Added some functions to make parsing more flexible. The count in disulfide_parser_pct is meant to be the total files you want to look at while the pct is the percentage of those files you want to look at to be parsed.

--- AIProtein/Disulfide_Parser.py
+++ AIProtein/Disulfide_Parser.py
@@ -1,12 +1,16 @@
-from dis import dis
 import geometry 
 import parsers 
 import util 
 import torch
 import os
 import numpy as np
+from dis import dis
 
-def disulfide_parser():
+"""
+Should have path variable to parameter but left blank for now to avoid messing up stuff running on your end
+add path to parameter when you make the proper changes
+"""
+def disulfide_parser(): 
     # can convert this into a function when the time comes
     # Get the list of all files and directories
     pdb_list = np.empty((0, 3, 3))
@@ -27,12 +31,79 @@ def disulfide_parser():
 
     # get ref dists
     c6d_ref = geometry.xyz_to_c6d(xyz_ref[None].permute(0,2,1,3),{'DMAX':20.0})
-    c6d_ref2 = geometry.xyz_to_c6d(xyz_ref[None],{'DMAX':20.0})
+    # c6d_ref2 = geometry.xyz_to_c6d(xyz_ref[None],{'DMAX':20.0})
     cys_c6d_ref = geometry.xyz_to_c6d(cys_xyz_ref[None].permute(0,2,1,3),{'DMAX':20.0})
-    cys_c6d_ref2 = geometry.xyz_to_c6d(cys_xyz_ref[None],{'DMAX':20.0})
+    # cys_c6d_ref2 = geometry.xyz_to_c6d(cys_xyz_ref[None],{'DMAX':20.0})
+    
     # 0 is not cys bond, 1 is cys bond
     return (0, c6d_ref.numpy()[0]), (1, cys_c6d_ref.numpy()[0])
 
+# Same parser as above but does percentage parsing
+# pct is a float from 0 to 1.0
+
+def disulfide_parser_pct(path, count, pct):
+    # can convert this into a function when the time comes
+    # Get the list of all files and directories
+    pdb_list = np.empty((0, 3, 3))
+    cys_pdb_list = np.empty((0, 3, 3))
+    total_to_parse = np.floor(count * pct)
+    counter = 0
+    
+    
+    # Make sure to change directory to where PDBs are
+    dir_list = os.listdir(path)
+    for x in dir_list:
+        if counter == total_to_parse:
+            break
+        if x.endswith(".pdb"):
+            new_f = path + "\\" + x
+            pdb = parsers.parse_pdb(new_f)
+            pdb_list = np.concatenate((pdb_list, pdb['xyz'][:,:3,:])) 
+            cys_pdb_list = np.concatenate((pdb_list, pdb['cys_xyz'][:,:3,:]))
+            counter += 1
+        
+
+    xyz_ref = torch.tensor(pdb_list).float()
+    cys_xyz_ref = torch.tensor(cys_pdb_list).float() 
+    # get ref dists
+    c6d_ref = geometry.xyz_to_c6d(xyz_ref[None].permute(0,2,1,3),{'DMAX':20.0})
+    cys_c6d_ref = geometry.xyz_to_c6d(cys_xyz_ref[None].permute(0,2,1,3),{'DMAX':20.0})
+    
+    # 0 is not cys bond, 1 is cys bond
+    return (0, c6d_ref.numpy()[0]), (1, cys_c6d_ref.numpy()[0])
+    
+# Count total files in pdb database
+def file_count(path):
+    count = 0
+    dir_list = os.listdir(path)
+    for x in dir_list:
+        if x.endswith(".pdb"):
+            count += 1
+    return count
+
+# Used to parse a specific file
+def specific_parse_file(path, file_name):
+    pdb_list = np.empty((0, 3, 3))
+    cys_pdb_list = np.empty((0, 3, 3))
+    new_f = path + "\\" + file_name
+    pdb = parsers.parse_pdb(new_f)
+    pdb_list = np.concatenate((pdb_list, pdb['xyz'][:,:3,:])) 
+    cys_pdb_list = np.concatenate((pdb_list, pdb['cys_xyz'][:,:3,:]))
+    
+    xyz_ref = torch.tensor(pdb_list).float()
+    cys_xyz_ref = torch.tensor(cys_pdb_list).float() 
+    # get ref dists
+    c6d_ref = geometry.xyz_to_c6d(xyz_ref[None].permute(0,2,1,3),{'DMAX':20.0})
+    cys_c6d_ref = geometry.xyz_to_c6d(cys_xyz_ref[None].permute(0,2,1,3),{'DMAX':20.0})
+    
+    # 0 is not cys bond, 1 is cys bond
+    return (0, c6d_ref.numpy()[0]), (1, cys_c6d_ref.numpy()[0])
+    
+# Used to update the pdb_set   
+def update_parser_db(parsed_pdb1, parsed_pdb2):
+    update_1 = (0, np.concatenate((parsed_pdb1[0][1], parsed_pdb2[0][1]), axis=0))
+    update_2 = (1, np.concatenate((parsed_pdb1[1][1], parsed_pdb2[1][1]), axis=0))
+    return update_1, update_2
 
 """
 Convert numpy format from (x, x, 4) to (y, 4) for neural network
