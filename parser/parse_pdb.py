a77247a37a1d20caaa2eea4064ef059f18f88f9f
Michael Reilly <michael-mbp@michaels-mbp.myfiosgateway.com>
2022-05-04 23:24:25 -0400

    Driver program implementation and parser debugging

--- parser/parse_pdb.py
+++ parser/parse_pdb.py
@@ -1,12 +1,15 @@
-import parsers
-import geometry
+import Parser.util.parser as parser
 import numpy as np
 
 def parse(filename):
-    pdb = parsers.parse_pdb(filename)
+    pdb = parser.parse_pdb(filename)
+    if pdb == []:
+        return []
     shape = pdb['idx'].shape[0]
+    # has_ssbond = 0
     for cys in pdb['ssbond']:
-        #(cat1, index1, cat2, index2, dist)
+        # cys shape: (cat1, index1, cat2, index2, dist)
+        # has_ssbond = 1
         index1 = np.where(pdb['idx'] == cys[1])[0]
         index2 = np.where(pdb['idx'] == cys[3])[0]
         for i in index1:
@@ -15,4 +18,5 @@ def parse(filename):
                 n2 = j*shape+i
                 pdb['c6d'][n1][4] = 1
                 pdb['c6d'][n2][4] = 1
+    # return (has_ssbond, pdb['c6d'])
     return pdb['c6d']
