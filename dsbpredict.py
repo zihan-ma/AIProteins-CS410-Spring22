1e76c376286aee673776b0c611e35e7bf3f421ff
Michael Reilly <michael-mbp@michaels-mbp.myfiosgateway.com>
2022-05-05 23:22:17 -0400

    added sorting on disulfide bonds

--- dsbpredict.py
+++ dsbpredict.py
@@ -1,16 +1,19 @@
+import numpy as np
 import subprocess
 import sys
 import os
 import argparse
-import Parser.parse_pdb as parser
-import numpy as np
 import gzip
+import shutil
+import Parser.parse_pdb as parser
 
 # directories
 cwd = os.getcwd()
 raw_fp = "/Data/Raw/"
 pdb_fp = "/Data/PDB/"
 parsed_fp = "/Data/Parsed/"
+have_ss_fp = "/Data/Have_SS/"
+no_ss_fp = "/Data/No_SS/"
 zip_ext = ".ent.gz"
 pdb_ext = ".pdb"
 parse_ext = ".csv"
@@ -20,11 +23,12 @@ argp = argparse.ArgumentParser()
 argp.add_argument("-d", "--download", action="store_true", help="check the PDB for updates, or download the PDB; zipped files are stored in Data/raw")
 argp.add_argument("-u", "--unzip", action="store_true", help="unzip the compressed downloaded PDB files; unzipped files are stored in Data/pdb")
 argp.add_argument("-p", "--parse", action="store_true", help="parse the pdb files; output files are stored in Data/parsed")
+argp.add_argument("-o", "--organize", action="store_true", help="sort parsed pdb files on existence of a disulfide bonds")
 argp.add_argument("-s", "--silent", action="store_true", help="suppress runtime information")
 args = argp.parse_args()
 
 # running
-if not (args.download or args.unzip or args.parse):
+if not (args.download or args.unzip or args.parse or args.organize):
     argp.print_help()
     exit(0)
 if args.download:
@@ -43,14 +47,14 @@ if args.unzip:
             continue
         name = pdb[:pdb.find(zip_ext)]
         fullname = name + pdb_ext
-        rawpath = cwd + raw_fp + pdb
-        pdbpath = cwd + pdb_fp + fullname
-        if not (fullname in unzipped and os.path.getmtime(rawpath) < os.path.getmtime(pdbpath)):
-            with gzip.open(rawpath, "rt") as infile:
+        raw_path = cwd + raw_fp + pdb
+        pdb_path = cwd + pdb_fp + fullname
+        if not (fullname in unzipped and os.path.getmtime(raw_path) < os.path.getmtime(pdb_path)):
+            with gzip.open(raw_path, "rt") as infile:
                 content = infile.read()
             if not args.silent:
                 print("unzipping", name)
-            with open(pdbpath, "w") as outfile:
+            with open(pdb_path, "w") as outfile:
                 outfile.write(content)
         else:
             if not args.silent:
@@ -65,13 +69,13 @@ if args.parse:
             continue
         name = pdb[:pdb.find(pdb_ext)]
         fullname = name + parse_ext
-        rawpath = cwd + raw_fp + name + zip_ext
-        pdbpath = cwd + pdb_fp + pdb
-        parsedpath = cwd + parsed_fp + fullname
-        if not (fullname in parsed and os.path.getmtime(rawpath) < os.path.getmtime(parsedpath)):
+        raw_path = cwd + raw_fp + name + zip_ext
+        pdb_path = cwd + pdb_fp + pdb
+        parsed_path = cwd + parsed_fp + fullname
+        if not (fullname in parsed and os.path.getmtime(raw_path) < os.path.getmtime(parsed_path)):
             if not args.silent:
                 print(name,end=" ")
-            data = parser.parse(pdbpath)
+            data = parser.parse(pdb_path)
             if np.array_equiv(np.array([1]), data) and not args.silent:
                 print("parse failed")
             elif np.array_equiv(np.array([2]), data) and not args.silent:
@@ -79,7 +83,36 @@ if args.parse:
             else:
                 if not args.silent:
                     print("parse successful")
-                np.savetxt(parsedpath, data, fmt=['%f','%f','%f','%f','%d','%s','%d','%s','%d'], delimiter=',')
+                np.savetxt(parsed_path, data, fmt=['%f','%f','%f','%f','%d','%s','%d','%s','%d'], delimiter=',')
         else:
             if not args.silent:
                 print(name, "already parsed")
+if args.organize:
+    os.makedirs(os.path.dirname(cwd + have_ss_fp), exist_ok=True)
+    os.makedirs(os.path.dirname(cwd + no_ss_fp), exist_ok=True)
+    parsed = os.listdir(cwd + parsed_fp)
+    have_ss = os.listdir(cwd + have_ss_fp)
+    no_ss = os.listdir(cwd + no_ss_fp)
+    for pdb in parsed:
+        if not pdb.endswith(parse_ext):
+            continue
+        name = pdb[:pdb.find(parse_ext)]
+        parsed_path = cwd + parsed_fp + pdb
+        have_ss_path = cwd + have_ss_fp + pdb
+        no_ss_path = cwd + no_ss_fp + pdb
+        pdb_data = np.loadtxt(parsed_path, dtype=parser.type, delimiter=',')
+        has_ss = False
+        if pdb_data.shape == ():
+            pdb_data = np.array([pdb_data])
+        for line in pdb_data:
+            if line[4] == 0:
+                has_ss = True
+                break
+        if has_ss:
+            if not args.silent:
+                print(name, "has a disulfide bond")
+            shutil.copyfile(parsed_path, have_ss_path)
+        else:
+            if not args.silent:
+                print(name, "has no disulfide bond")
+            shutil.copyfile(parsed_path, no_ss_path)
